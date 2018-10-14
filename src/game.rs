1f9f30c2d58433d9642186953f0fe4d3e31adcea
Destructed3 <hunzi.han@gmail.com>
2018-10-14 12:45:44 +0200

    finished find_start_nodes

--- src/game.rs
+++ src/game.rs
@@ -32,19 +32,18 @@ fn setup_players(mut gd: GameData) -> GameData {
             gd.add_exec_to_player(&id, &player_id);
         }
         
-        let node_id = find_start_node(&gd);
+        let node_id = find_start_node(&gd).expect("{}");
         gd.add_node_to_player(&node_id, &player_id);
     }
     gd
 }
 
-fn find_start_node(gd: &GameData) -> String {
+fn find_start_node(gd: &GameData) -> Result<String, String> {
     let mut node_id: &str = "";
     let rows = gd.map.iter();
     for row in rows {
         let nodes = row.iter();
         for node in nodes {
-            // Heavily oversimplified
             if node_allowed(&gd, &node) {
                 node_id = &node.id;
                 break;
@@ -55,9 +54,9 @@ fn find_start_node(gd: &GameData) -> String {
         }
     }
     if node_id == "" {
-        panic!("Couldn't find fitting startnode");
+        return Err(String::from("Fuck"));
     }
-    node_id.to_owned()
+    return Ok(node_id.to_owned())    
 }
 
 fn find_start_execs(gd: &GameData) -> Vec<String> {
@@ -79,27 +78,21 @@ fn find_start_execs(gd: &GameData) -> Vec<String> {
 fn node_allowed(gd: &GameData, node: &Node) -> bool {
     // Ownerless
     if node.owner != "" {
-        return false
+        return false;
     }
     // Away from map-boarders
-    if node.y < 2 && node.y > gd.map.len() as u32 -1 {
+    if node.y < 2 || node.y > gd.map.len() as u32 -3 {
         return false;
     }
-    if node.x < 2 && node.x > gd.map[0].len() as u32 -1 {
+    if node.x < 2 || node.x > gd.map[0].len() as u32 -3 {
         return false;
     }
-    // Distance to next owned node
+    let check_blocker = |old_node: &Node| {
+        old_node.owner != "" && (node.x < old_node.x+3) && (node.y < old_node.y+3)
+    };
     for row in gd.map.iter() {
-        for _node in row.iter() {
-            if (node.x < 2 && _node.x < 4) || (node.y < 2 && _node.y < 4) {
-                return false;
-            }
-            if node.x + 2 < _node.x && node.x - 2 > _node.x {
-                return false;
-            }
-            if node.y + 2 < _node.y && node.y - 2 > _node.y {
-                return false
-            }
+        if row.iter().filter( |n| check_blocker(n) ).count() > 0 {
+            return false;
         }
     }
     true
@@ -125,16 +118,19 @@ mod tests {
     #[test]
     fn test_find_start_node() {
         use std::collections::HashMap;
+        // Config
+        let nodes_per_length = |length: f32, space_to_boarder: f32, space_between: f32| { return ((length - 2_f32*space_to_boarder) / (space_between+1_f32)).ceil() };
+        let (map_len, map_hei, space_to_boarder, space_between) = (8, 8, 2, 2); // To be moved to config-struct
+        let nodes_per_row = nodes_per_length(map_len as f32, space_to_boarder as f32, space_between as f32) as usize;
+        let nodes_per_col = nodes_per_length(map_hei as f32, space_to_boarder as f32, space_between as f32) as usize;
+        let nr_nodes = nodes_per_row * nodes_per_col;
 
-        let mut game_data = objects::game_data::GameData::new(&vec![16,16]);
-        game_data = setup_players(game_data);
+        let mut game_data = objects::game_data::GameData::new(&vec![map_len,map_hei]);
         let player_id = game_data.players[0].id.to_owned();
-        let map_len = game_data.map[0].capacity() as u32;
-        let map_hei = game_data.map.capacity() as u32;
-        let nr_nodes = 4;
         let mut node_coord: Vec<HashMap<&str, u32>> = Vec::with_capacity(nr_nodes);
+        // Test starts here
         while node_coord.len() < node_coord.capacity() {
-            let node_id = find_start_node(&game_data);
+            let node_id = find_start_node(&game_data).expect("{}");
             let node = game_data.get_node(&node_id);
             // Ensure real node without owner
             assert_eq!(node.owner, "", "Node nr {}", node_coord.len()+1);
@@ -149,22 +145,30 @@ mod tests {
             let x = node.get("x").unwrap();
             let y = node.get("y").unwrap();
             // Node 2 rows and cols from map-boarders away
-            assert!( x > &(1), "X should be > 1 -> x: {}", x );
-            assert!( x < &( map_len - 1), "X should be < {} -> x: {}", map_len - 1, x);
-            assert!( y > &(1), "Y should be > 1 -> y: {}", y );
-            assert!( y < &(map_hei - 1), "Y should be < {} -> y: {}",(map_hei - 1), y );
+            assert!( x > &(1), "X should be > {} -> x: {} | {:?}", space_to_boarder-1, x, node_coord );
+            assert!( x < &( map_len - 1), "X should be < {} -> x: {} | {:?}", map_len - 1, x, node_coord);
+            assert!( y > &(1), "Y should be > {} -> y: {}, | {:?}", space_to_boarder-1, y, node_coord );
+            assert!( y < &(map_hei - 1), "Y should be < {} -> y: {} | {:?}",(map_hei - 1), y, node_coord );
             // Node at least 2 rows and cols from next player-owned node
-            let close_nodes_x = node_coord.iter().map(|n| n.get("x").unwrap() < &(x+2) && n.get("x").unwrap() > &(x-2)).count();
-            let close_nodes_y = node_coord.iter().map(|n| n.get("y").unwrap() < &(y+2) && n.get("y").unwrap() > &(y-2)).count();
-            assert!( close_nodes_x < 2, "Owned starting nodes to close to each other (x-row) -> {} nodes too close for {}/{}", close_nodes_x, x, y );
-            assert!( close_nodes_y < 2, "Owned starting nodes to close to each other (y-row) -> {} nodes too close", close_nodes_y );
+            let check = |n: &HashMap<&str, u32>, arg: &str| {
+                let coord = node.get(arg).unwrap();
+                let _coord = n.get(arg).unwrap();
+                if _coord < &(space_to_boarder+1)  {
+                    return coord > &(space_to_boarder+space_between+2); // +2 because you need to account for 2 nodes
+                } else {
+                    return coord > &(_coord+space_between+1) && coord < &(_coord-space_between-1);
+                }
+            };
+            let close_nodes_x = node_coord.iter().filter( |n| check(n, "x") ).count();
+            let close_nodes_y = node_coord.iter().filter( |n| check(n, "y") ).count();
+            assert_eq!( close_nodes_x, 0, "Owned starting nodes to close to each other (x-row) -> {} nodes too close, {} Nodes required; {:?}", close_nodes_x, nr_nodes, node_coord );
+            assert_eq!( close_nodes_y, 0, "Owned starting nodes to close to each other (y-row) -> {} nodes too close, {} Nodes required; {:?}", close_nodes_y, nr_nodes, node_coord );
         }
     }
 
     #[test]
     fn test_find_start_execs() {
         let mut game_data = objects::game_data::GameData::new(&vec![16,16]);
-        game_data = setup_players(game_data);
         let exec_ids = find_start_execs(&game_data);
         let execs_ids_iter = exec_ids.iter();
         for exec_id in execs_ids_iter {
